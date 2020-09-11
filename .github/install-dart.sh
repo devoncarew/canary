ac84741b1c937b2c9ba6cd599d63302ae711fb03
Devon Carew <devoncarew@google.com>
2020-09-10 18:58:14 -0700

    download the sdk

--- .github/install-dart.sh
+++ .github/install-dart.sh
@@ -7,20 +7,16 @@ echo "hello world"
 
 echo Installing Dart ${DART_VERSION}
 
-# sh.cmd "curl --connect-timeout 15 --retry 5 #{archive_url}/sdk/dartsdk-#{os}-x64-release.zip > ${TRAVIS_HOME}/dartsdk.zip"
-# sh.cmd "unzip ${TRAVIS_HOME}/dartsdk.zip -d ${TRAVIS_HOME} > /dev/null"
-# sh.cmd "rm ${TRAVIS_HOME}/dartsdk.zip"
-# sh.cmd 'export DART_SDK="${TRAVIS_HOME}/dart-sdk"'
-# sh.cmd 'export PATH="$DART_SDK/bin:$PATH"'
-# sh.cmd 'export PATH="${TRAVIS_HOME}/.pub-cache/bin:$PATH"'
+curl --connect-timeout 15 --retry 5 \
+  https://storage.googleapis.com/dart-archive/channels/${DART_VERSION}/sdk/dartsdk-linux-x64-release.zip > dartsdk.zip
+unzip dartsdk.zip -d dart-sdk > /dev/null
+rm dartsdk.zip
 
+export DART_SDK="`pwd`/dart-sdk"
+export PATH="$DART_SDK/bin:$PATH"
+export PATH="${HOME}/.pub-cache/bin:$PATH"
 
-# const dartChannel = core.getInput("channel", { required: true });
-# const releaseType = dartChannel === "be" ? "raw" : "release";
-# const url = `https://storage.googleapis.com/dart-archive/channels/${dartChannel}/${releaseType}/latest/sdk/dartsdk-${dartOS}-x64-release.zip`;
+echo $HOME
 
-# const dartZipPath = await tc.downloadTool(url);
-
-# // TODO: Cache?
-# // https://github.com/actions/toolkit/tree/master/packages/tool-cache
-# const dartSdkPath = await tc.extractZip(dartZipPath);
+echo $DART_SDK
+echo $PATH
