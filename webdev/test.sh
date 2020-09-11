bc0d82bbdf694a87a3b4eeb75633f0f0fbb6d617
Devon Carew <devoncarew@google.com>
2020-09-10 22:57:42 -0700

    add additional testing around webdev (#8)
    add additional testing around webdev

--- webdev/test.sh
+++ webdev/test.sh
@@ -3,5 +3,17 @@
 # Fast fail the script on failures.
 set -ex
 
-# todo: improve this
+# Install webdev.
 pub global activate webdev
+
+# Ensure the build packages can solve against the SDK.
+pub get
+
+# Ensure that the project analyses cleanly.
+dartanalyzer --fatal-infos .
+
+# Drive a build.
+webdev build
+
+# Also perform a build through build_runner.
+pub run build_runner build
